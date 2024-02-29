9fe57cfc01e2c2b1e5a6d4e60977b5c75da41b25
Stormo <lorenzomanuel_ante@yahoo.com>
2024-02-29 14:37:00 +1100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const axios = require('axios');
-
 /**
  * Generates a random integer between min (inclusive) and max (inclusive).
  * @param {number} min - Minimum value.
@@ -7,7 +6,6 @@ const axios = require('axios');
  * @returns {number} - Random integer.
  */
 function getRandomInt(min, max) {
-  const response = axios.get('https://aws.random.cat/meow');
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
